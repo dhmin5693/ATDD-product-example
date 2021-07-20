c2d6275bc094a61d1a1af56cb34023f812ac8f69
dhmin5693 <dhmin5693@naver.com>
2021-07-20 23:47:01 +0900

    first commit

---
+++ src/main/java/min/study/atddproduct/AtddProductApplication.java
@@ -0,0 +1,13 @@
+package min.study.atddproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtddProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtddProductApplication.class, args);
+    }
+
+}
---
+++ src/test/java/min/study/atddproduct/AtddProductApplicationTests.java
@@ -0,0 +1,13 @@
+package min.study.atddproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtddProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
